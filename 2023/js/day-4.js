4a62d7e77862de4bb7cdfa0290618c5f0dd72aea
Dima Pasumansky <pasumansky@jetstyle.ru>
2024-07-31 22:14:59 +0500

    day 4 part 1 done

--- 2023/js/day-4.js
+++ 2023/js/day-4.js
@@ -5,22 +5,41 @@ const INPUT = './inputFiles/input4.txt'
 const file = fs.readFileSync(INPUT, { encoding: 'utf8' })
 const lines = file.split('\n')
 
+lines.pop() // pop last empty line
+
 let totalPoints = 0
 
 for (const line of lines) {
-  const points = calculateCardPoints(line)
-  console.log('points @ ', points)
-  break
+  const [winningNumbers, givenNumbers] = splitCardNumbers(line)
+  // console.log('winningNumbers @ ', winningNumbers)
+  // console.log('givenNumbers @ ', givenNumbers)
+  
+  const points = calculatePoints(winningNumbers, givenNumbers)
+  // console.log('points @ ', points)
+  totalPoints+=points
 }
 
-function calculateCardPoints(line) {
+console.log('total points @ ', totalPoints)
+
+function splitCardNumbers(line) {
   const re = /Card\s+\d+:\s*((?:\d{1,2}\s+){10})\s*\|\s*((?:\d{1,2}\s*)+)/
   const match = line.match(re)
-
+  
   const winningNumbers = match[1].trim().split(/\s+/)
   const givenNumbers = match[2].trim().split(/\s+/)
-  console.log('winningNumbers @ ', winningNumbers)
-  console.log('givenNumbers @ ', givenNumbers)
   
-  return 0
+  return [winningNumbers, givenNumbers]
+}
+
+function calculatePoints(winningNumbers, givenNumbers) {
+  let points = 0
+  for (const winningNum of winningNumbers) {
+    for (const givenNum of givenNumbers) {
+      if (winningNum === givenNum) {
+        if (points === 0) points++
+        else points = points * 2
+      }
+    }
+  }
+  return points
 }
